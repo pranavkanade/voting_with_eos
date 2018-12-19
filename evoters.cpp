f9fc2f64b6d275b25355a39e65497927f90145d6
pskanade <k.pranav.apk@gmail.com>
2018-12-19 12:09:29 +0530

    Added the evoter contract

---
+++ evoters.cpp
@@ -0,0 +1,72 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <string>
+
+using namespace std;
+using namespace eosio;
+
+class [[eosio::contract]] evoters : public contract {
+    public:
+        using contract::contract;
+
+        evoters(
+            name user,
+            name code,
+            datastream<const char*> ds
+        ): contract(user, code, ds) {}
+
+        // upsert
+        [[eosio::action]]
+        void upsert(
+            name user,
+            uint64_t age,
+            uint64_t aadhar_no;
+            uint64_t uid
+        ) {
+            // check if correct user has called out the action
+            require_auth(user);
+            // Create a handle to the table below
+            voters_index voters(_code, _code.value);
+            // Find if the user already exists in the table
+            auto iterator = voters.find(user.value);
+
+            if (iterator == voters.end()) {
+                // table does not have user info
+                voters.emplace(
+                    user,
+                    [&](auto& row) {
+                        row.key = user;
+                        row.age = age;
+                        row.aadhar_no = aadhar_no;
+                        row.uid = uid;
+                    }
+                );
+            }
+            else {
+                // table does have the user info
+                voters.modify(
+                    user,
+                    [&](auto& row) {
+                        row.age = age;
+                        row.aadhar_no = aadhar_no;
+                        row.uid = uid;
+                    }
+                );
+            }
+        }
+    private:
+        // Create a table to hold the info regarding all the voters
+        struct [[eosio::table]] voter {
+            name key;
+            uint64_t age;
+            uint64_t aadhar_no;
+            uint64_t uid;       // other than aadhar
+
+            uint64_t primary_key() { return key.value; }
+        }
+
+        typedef multi_index<
+            "voters"_n,
+            voter
+        > voters_index;
+}
